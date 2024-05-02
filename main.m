68e72593bd092f475d8f24e56a7a33c33de48f42
Shiyu-H-Cheng <scheng3@wpi.edu>
2024-05-02 17:55:02 -0500

    Adding system topology

--- main.m
+++ main.m
@@ -1,1 +1,15 @@
 % the system topology
+% generate adjacency matrix
+num_N = 7; % number of nodes in G
+edges = [1 2; 1 4; 2 3; 3 4; 3 6;4 5; 5 7; 6 7;];
+adjacency_G = zeros(num_N);
+for i = 1: size(edges, 1)
+    temp_edge = edges(i,:);
+    adjacency_G(temp_edge(1), temp_edge(2)) = 1;
+    adjacency_G(temp_edge(2), temp_edge(1)) = 1;
+end
+degree_G = zeros(num_N);
+for i = 1: num_N
+    degree_G(i,i) = sum(adjacency_G(i,:));
+end
+laplacian_G = degree_G - adjacency_G;
